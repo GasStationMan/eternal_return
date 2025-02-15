889d6a80392bfefb3a9efc56cc0a0175ee425082
TDanfung <rystuziop@gmail.com>
2025-02-16 03:53:17 +0900

    리팩토링

--- eternal_return/src/main/java/org/EternalReturn/System/ERPlayer/ERPlayerScript.java
+++ eternal_return/src/main/java/org/EternalReturn/System/ERPlayer/ERPlayerScript.java
@@ -26,64 +26,15 @@ public class ERPlayerScript implements Script {
 
             //보스바 gui업데이트
             if(erPlayer.isKioskGuiOpened()){
-                updateMouseCursor(erPlayer, erPlayer.getKioskGui());
+                erPlayer.getKioskGui().updateMouseCursor(erPlayer);
             }
             else if(erPlayer.isHyperloopGuiOpened()){
-                updateMouseCursor(erPlayer, erPlayer.getHyperloop());
+                erPlayer.getHyperloopGui().updateMouseCursor(erPlayer);
             }
         }
     }
 
-    /**
-     * 마우스커서 위치 업데이트 기능만 따로 뺀 함수 <br>
-     * getDegreeDistance(Vec2d secondVector, Vec2d currentVector)함수를 사용함. <br>
-     * 디버그 함수 erPlayer.sendMessage()가 주석 처리 되어 있는 지 확인할 것.
-     * @return void
-     * */
-    private void updateMouseCursor(ERPlayer erPlayer, BossbarGuiFrame gui){
-        Player p = erPlayer.getPlayer();
-        Location location = p.getLocation();
-        float yaw = location.getYaw();
-        float pitch = location.getPitch() * 4;
 
-        Vec2d currentVecX = erPlayer.getRot2dVecX();
-        Vec2d secondVecX = new Vec2d(
-                Math.cos(Math.toRadians(yaw)),
-                Math.sin(Math.toRadians(yaw))
-        );
-        double distanceOfAngleX = getDegreeDistance(secondVecX,currentVecX);
-
-        Vec2d currentVecY = erPlayer.getRot2dVecY();
-        Vec2d secondVecY = new Vec2d(
-                Math.cos(Math.toRadians(pitch)),
-                Math.sin(Math.toRadians(pitch))
-        );
-        double distanceOfAngleY = getDegreeDistance(secondVecY,currentVecY);
-
-        if(0.0009765625d <= Math.abs(distanceOfAngleX) || 0.0009765625d <= Math.abs(distanceOfAngleY)){
-
-            gui.moveMousePointer((int)distanceOfAngleX, (int)distanceOfAngleY);
-
-            if(Math.abs(pitch) == 360){
-                location.setPitch(-pitch);
-                p.teleport(location);
-            }
-
-            erPlayer.setRot2dVecX(secondVecX);
-            erPlayer.setRot2dVecY(secondVecY);
-
-        }
-    }
-
-    /**
-     * 두 벡터의 각도의 크기와 방향을 같이 반환.
-     * @return 두 벡터간 Degree 값 * 외적방향
-     * */
-    private double getDegreeDistance(Vec2d secondVector, Vec2d currentVector){
-        double direction = currentVector.crossProd(secondVector);
-        double distanceOfAngle = Math.toDegrees(Vec2d.getAngleOfVectors(secondVector, currentVector));
-        return direction > 0 ? distanceOfAngle : -distanceOfAngle;
-    }
 
 
 
--- eternal_return/src/main/java/org/EternalReturn/Util/Gui/bossbarGui/Model/BossbarGuiFrame.java
+++ eternal_return/src/main/java/org/EternalReturn/Util/Gui/bossbarGui/Model/BossbarGuiFrame.java
@@ -93,8 +93,6 @@ public class BossbarGuiFrame {
         }
     }
 
-
-
     /**
      * BossbarGui객체가 가지고 있는 bComponent 리스트를 kyori component리스트로 바꾸어 <br>
      * 다시 보스바의 이름 버퍼로 표시하는 함수
@@ -158,6 +156,55 @@ public class BossbarGuiFrame {
         return isOpen;
     }
 
+    /**
+     * 마우스커서 위치 업데이트 기능만 따로 뺀 함수 <br>
+     * getDegreeDistance(Vec2d secondVector, Vec2d currentVector)함수를 사용함. <br>
+     * 디버그 함수 erPlayer.sendMessage()가 주석 처리 되어 있는 지 확인할 것.
+     * @return void
+     * */
+    public void updateMouseCursor(ERPlayer erPlayer){
+        Player p = erPlayer.getPlayer();
+        Location location = p.getLocation();
+        float yaw = location.getYaw();
+        float pitch = location.getPitch() * 4;
 
+        Vec2d currentVecX = erPlayer.getRot2dVecX();
+        Vec2d secondVecX = new Vec2d(
+                Math.cos(Math.toRadians(yaw)),
+                Math.sin(Math.toRadians(yaw))
+        );
+        double distanceOfAngleX = getDegreeDistance(secondVecX,currentVecX);
+
+        Vec2d currentVecY = erPlayer.getRot2dVecY();
+        Vec2d secondVecY = new Vec2d(
+                Math.cos(Math.toRadians(pitch)),
+                Math.sin(Math.toRadians(pitch))
+        );
+        double distanceOfAngleY = getDegreeDistance(secondVecY,currentVecY);
+
+        if(0.0009765625d <= Math.abs(distanceOfAngleX) || 0.0009765625d <= Math.abs(distanceOfAngleY)){
+
+            moveMousePointer((int)distanceOfAngleX, (int)distanceOfAngleY);
+
+            if(Math.abs(pitch) == 360){
+                location.setPitch(-pitch);
+                p.teleport(location);
+            }
+
+            erPlayer.setRot2dVecX(secondVecX);
+            erPlayer.setRot2dVecY(secondVecY);
+
+        }
+    }
+
+    /**
+     * 두 벡터의 각도의 크기와 방향을 같이 반환.
+     * @return 두 벡터간 Degree 값 * 외적방향
+     * */
+    private double getDegreeDistance(Vec2d secondVector, Vec2d currentVector){
+        double direction = currentVector.crossProd(secondVector);
+        double distanceOfAngle = Math.toDegrees(Vec2d.getAngleOfVectors(secondVector, currentVector));
+        return direction > 0 ? distanceOfAngle : -distanceOfAngle;
+    }
 
 }
